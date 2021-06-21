2c4a079f580d674eed08f91ed2d843521ba26949
jcohy <jia_chao23@126.com>
2021-06-22 00:19:56 +0800

    添加 jcohy-gradle-plugins

---
+++ jcohy-java-convention/src/main/java/com/jcohy/convention/version/ReleaseStatus.java
@@ -0,0 +1,55 @@
+package com.jcohy.convention.version;
+
+import java.util.regex.Pattern;
+
+import org.gradle.api.Project;
+
+import org.springframework.util.Assert;
+
+/**
+ * Copyright: Copyright (c) 2021
+ * <a href="http://www.jcohy.com" target="_blank">jcohy.com</a>
+ *
+ * <p>
+ * Description: Release 版本状态
+ *
+ * @author jiac
+ * @version 1.0.0 2021/6/17:15:51
+ * @since 1.0.0
+ */
+public enum ReleaseStatus {
+
+	/**
+	 * 快照版本
+	 */
+	SNAPSHOT,
+	/**
+	 * 预发布版本
+	 */
+	PRERELEASE,
+	/**
+	 * 可用版本
+	 */
+	GENERAL_AVAILABILITY;
+
+	private static final Pattern PRERELEASE_PATTERN = Pattern.compile("[A-Za-z0-9\\.\\-]+?(M|RC)\\d+");
+
+	private static final String SNAPSHOT_SUFFIX = "SNAPSHOT";
+
+	/**
+	 * 根据版本名 获取 {@link ReleaseStatus status}
+	 * @param project 项目版本
+	 * @return 获取此项目的 release status
+	 */
+	public static ReleaseStatus ofProject(Project project) {
+		String version = project.getVersion().toString();
+		Assert.notNull(version, "Version must not be null");
+		if (version.endsWith(SNAPSHOT_SUFFIX)) {
+			return SNAPSHOT;
+		}
+		if (PRERELEASE_PATTERN.matcher(version).matches()) {
+			return PRERELEASE;
+		}
+		return GENERAL_AVAILABILITY;
+	}
+}
