ea95f8636fb640888aab5a0f0ffeba8de5cdb4b9
jiac <jia_chao23@126.com>
2021-07-17 11:43:09 +0800

    将 java，groovy，kotlin 源码复制到 build 目录下

---
+++ jcohy-java-convention/src/test/gradle/src/main/java/com/jcohy/gradle/JavaExample.java
@@ -0,0 +1,15 @@
+/**
+ * <p> 描述: .
+ * Copyright: Copyright (c) 2021.
+ * <a href="http://www.jcohy.com" target="_blank">jcohy.com</a>
+ *
+ * @author jiac
+ * @version 1.0.0 2021/7/15:16:38
+ * @since 1.0.0
+ */
+public class JavaExample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!!!");
+	}
+}
