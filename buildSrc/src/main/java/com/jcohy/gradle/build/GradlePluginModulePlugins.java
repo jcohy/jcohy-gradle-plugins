2d054ee85608fd41edeb36aef3b747c5beeae6b5
jcohy <jia_chao23@126.com>
2023-02-17 02:06:19 +0800

    1、升级版本 spring-framework 6.0.2 2、升级版本 spring-framework 6.0.2 3、升级版本 assertj-core 3.23.1 4、升级版本 assertj-core 5.9.1 5、Maven DSL 重写 3、删除无用依赖项

--- buildSrc/src/main/java/com/jcohy/gradle/build/GradlePluginModulePlugins.java
+++ buildSrc/src/main/java/com/jcohy/gradle/build/GradlePluginModulePlugins.java
@@ -2,7 +2,8 @@ package com.jcohy.gradle.build;
 
 
 import java.net.URI;
-import java.util.Collections;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import io.spring.gradle.dependencymanagement.DependencyManagementPlugin;
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension;
@@ -11,14 +12,17 @@ import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.ConfigurationContainer;
 import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.artifacts.dsl.DependencyHandler;
-import org.gradle.api.internal.java.DefaultJavaPlatformExtension;
-import org.gradle.api.plugins.ExtensionContainer;
+import org.gradle.api.logging.LogLevel;
 import org.gradle.api.plugins.JavaLibraryPlugin;
-import org.gradle.api.plugins.JavaPlatformExtension;
+import org.gradle.api.plugins.JavaPlatformPlugin;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.PluginContainer;
-import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.javadoc.Javadoc;
+import org.gradle.api.tasks.testing.Test;
+import org.gradle.external.javadoc.JavadocMemberLevel;
+import org.gradle.jvm.tasks.Jar;
 import org.gradle.plugin.devel.plugins.JavaGradlePluginPlugin;
 
 /**
@@ -39,13 +43,75 @@ public class GradlePluginModulePlugins implements Plugin<Project> {
 		if(project.getName().startsWith("jcohy-")) {
 			plugins.apply(JavaLibraryPlugin.class);
 			plugins.apply(JavaGradlePluginPlugin.class);
-			plugins.apply(MavenPublishPlugin.class);
 			project.setProperty("sourceCompatibility", "17");
 			project.setProperty("targetCompatibility", "17");
+			configureTestConventions(project);
 			configureMavenRepository(project);
+			configureJarManifest(project);
+			configureJavadoc(project);
 			configureDependencyManagement(project);
 		}
 
+	}
+
+	private void configureTestConventions(Project project) {
+		project.getTasks().withType(Test.class, (test) -> {
+			test.useJUnitPlatform();
+			test.setMaxHeapSize("1024M");
+		});
+		project.getPlugins().withType(JavaPlugin.class, javaPlugin -> {
+			project.getDependencies().add(JavaPlugin.TEST_RUNTIME_ONLY_CONFIGURATION_NAME, "org.junit.jupiter:junit-jupiter-engine");
+			project.getDependencies().add(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME, "org.junit.jupiter:junit-jupiter-api");
+			project.getDependencies().add(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME, "org.assertj:assertj-core");
+			project.getDependencies().add(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME, "org.springframework.boot:spring-boot-starter-test");
+		});
+
+	}
+	private void configureJarManifest(Project project) {
+
+		SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
+		Set<String> sourceJarTaskNames = sourceSets.stream().map(SourceSet::getSourcesJarTaskName).collect(Collectors.toSet());
+		Set<String> javadocJarTaskNames = sourceSets.stream().map(SourceSet::getJavadocJarTaskName).collect(Collectors.toSet());
+
+		project.getTasks().withType(Jar.class, jar -> project.afterEvaluate((evaluated) -> {
+			jar.manifest(manifest -> {
+				Map<String, Object> attributes = new TreeMap<>();
+				attributes.put("Automatic-Module-Name", project.getName().replace("-", "."));
+				attributes.put("Build-Jdk-Spec", project.property("sourceCompatibility"));
+				attributes.put("Built-By", "Jcohy");
+				attributes.put("Implementation-Title",
+						determineImplementationTitle(project, sourceJarTaskNames, javadocJarTaskNames, jar));
+				attributes.put("Implementation-Version", project.getVersion());
+				manifest.attributes(attributes);
+			});
+		}));
+	}
+
+	private void configureJavadoc(Project project) {
+		project.getTasks().withType(Javadoc.class, javadoc -> {
+			javadoc.setDescription("Generates project-level javadoc for use in -javadoc jar");
+			javadoc.options((option) -> {
+				option.encoding("UTF-8");
+				option.source("17");
+				option.setMemberLevel(JavadocMemberLevel.PROTECTED);
+				option.header(project.getName());
+			});
+			// 跨模块的 @see 和 @link 引用消除警告
+			javadoc.getLogging().captureStandardError(LogLevel.INFO);
+			// 消除 "## warnings" 消息
+			javadoc.getLogging().captureStandardOutput(LogLevel.INFO);
+		});
+	}
+
+	private String determineImplementationTitle(Project project, Set<String> sourceJarTaskNames,
+												Set<String> javadocJarTaskNames, Jar jar) {
+		if (sourceJarTaskNames.contains(jar.getName())) {
+			return "Source for " + project.getName();
+		}
+		if (javadocJarTaskNames.contains(jar.getName())) {
+			return "Javadoc for " + project.getName();
+		}
+		return project.getDescription();
 	}
 
 	private void configureMavenRepository(Project project) {
@@ -76,14 +142,16 @@ public class GradlePluginModulePlugins implements Plugin<Project> {
 			configuration.setVisible(false);
 			configuration.setCanBeConsumed(false);
 			configuration.setCanBeResolved(false);
+			Dependency parent = project.getDependencies().enforcedPlatform(project.getDependencies()
+					.platform(Collections.singletonMap("path", ":bom")));
+			configuration.getDependencies().add(parent);
 		});
 
+		project.getDependencies().add(JavaPlatformPlugin.API_CONFIGURATION_NAME, ":bom");
 		project.getPlugins().apply(DependencyManagementPlugin.class);
 
 		DependencyManagementExtension dependencyManagementExtension = project.getExtensions().getByType(DependencyManagementExtension.class);
-		dependencyManagementExtension.imports((importsHandler -> {
-			importsHandler.mavenBom("org.springframework.boot:spring-boot-dependencies:3.0.0");
-		}));
+		dependencyManagementExtension.imports((importsHandler -> importsHandler.mavenBom("org.springframework.boot:spring-boot-dependencies:3.0.0")));
 
 		configurations
 				.matching((configuration) ->
@@ -93,12 +161,8 @@ public class GradlePluginModulePlugins implements Plugin<Project> {
 				});
 
 //		project.getDependencies().getConstraints().add(JavaPlugin.API_CONFIGURATION_NAME,"org.asciidoctor:asciidoctor-gradle-jvm:3.3.2");
-		Dependency parent = project.getDependencies().enforcedPlatform(project.getDependencies()
-				.project(Collections.singletonMap("path", ":bom")));
+//
 
-		project.getConfigurations().getByName("dependencyManagement", (dependency) -> {
-			dependency.getDependencies().add(parent);
-		});
 
 	}
 }
---
+++ buildSrc/src/main/java/com/jcohy/gradle/build/GradlePluginPublishPlugins.java
@@ -0,0 +1,175 @@
+package com.jcohy.gradle.build;
+
+import com.jcohy.gradle.build.deploy.PomConstant;
+import com.jcohy.gradle.build.deploy.Repository;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.attributes.Usage;
+import org.gradle.api.plugins.*;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.VariantVersionMappingStrategy;
+import org.gradle.api.publish.maven.*;
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
+import org.gradle.plugins.signing.SigningExtension;
+import org.gradle.plugins.signing.SigningPlugin;
+import org.springframework.util.StringUtils;
+
+public class GradlePluginPublishPlugins implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        PluginContainer plugins = project.getPlugins();
+        plugins.apply(JavaLibraryPlugin.class);
+        plugins.apply(MavenPublishPlugin.class);
+        plugins.apply(SigningPlugin.class);
+        configureMavenPublish(project);
+    }
+
+
+    private void configureMavenPublish(Project project) {
+        project.getPlugins().withType(MavenPublishPlugin.class).all((mavenPublishPlugin) -> {
+            PublishingExtension publishing = project.getExtensions().getByType(PublishingExtension.class);
+            publishing.getRepositories().maven(mavenRepository -> {
+                Repository repository = Repository.ofProject(project);
+                mavenRepository.setUrl(repository.getUrl());
+                mavenRepository.setName(repository.getName());
+                mavenRepository.credentials((passwordCredentials -> {
+                    String username = StringUtils.hasText(getExtraProperties(project,"username")) ? getExtraProperties(project,"username") : PomConstant.NEXUS_USER_NAME;
+                    String password = StringUtils.hasText(getExtraProperties(project,"password")) ? getExtraProperties(project,"password") : PomConstant.NEXUS_PASSWORD;
+                    passwordCredentials.setUsername(username);
+                    passwordCredentials.setPassword(password);
+                }));
+            });
+
+            publishing.getPublications().withType(MavenPublication.class)
+                    .all(((mavenPublication) -> {
+                        customizeMavenPublication(mavenPublication, project);
+                        SigningExtension extension = project.getExtensions().getByType(SigningExtension.class);
+                        extension.sign(mavenPublication);
+                            }
+                        ));
+
+            project.getPlugins().withType(JavaPlugin.class).all(javaPlugin -> {
+                JavaPluginExtension extension = project.getExtensions().getByType(JavaPluginExtension.class);
+                extension.withJavadocJar();
+                extension.withSourcesJar();
+            });
+        });
+    }
+
+    public String getExtraProperties(Project project,String property){
+        ExtraPropertiesExtension extra = project.getExtensions().getExtraProperties();
+        return extra.has(property) ? (String) extra.get(property) : "";
+    }
+
+    /**
+     * 自定义 maven 满足 Maven Central 的要求
+     *
+     * @param publication publication
+     * @param project project
+     */
+    private void customizeMavenPublication(MavenPublication publication, Project project) {
+        customizePom(publication.getPom(), project);
+        project.getPlugins().withType(JavaPlugin.class)
+                .all(javaPlugin -> customizeJavaMavenPublication(publication, project));
+        suppressMavenOptionalFeatureWarnings(publication);
+    }
+
+    /**
+     * 添加发布兼容性警告
+     *
+     * @param publication publication
+     */
+    private void suppressMavenOptionalFeatureWarnings(MavenPublication publication) {
+        publication.suppressPomMetadataWarningsFor("mavenOptionalApiElements");
+        publication.suppressPomMetadataWarningsFor("mavenOptionalRuntimeElements");
+    }
+
+    /**
+     * 版本解析
+     *
+     * @param publication publication
+     * @param project project
+     */
+    private void customizeJavaMavenPublication(MavenPublication publication, Project project) {
+        publication.versionMapping((strategy) -> strategy.usage(Usage.JAVA_API, (mappingStrategy) -> mappingStrategy
+                .fromResolutionOf(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME)));
+        publication.versionMapping((strategy) -> strategy.usage(Usage.JAVA_RUNTIME,
+                VariantVersionMappingStrategy::fromResolutionResult));
+    }
+
+    /**
+     * 定义 pom 文件
+     *
+     * @param pom pom
+     * @param project project
+     */
+    private void customizePom(MavenPom pom, Project project) {
+        pom.getUrl().set(PomConstant.GIT_URL);
+        pom.getName().set(project.provider(project::getName));
+        pom.getDescription().set(project.provider(project::getDescription));
+        pom.organization(this::customizeOrganization);
+
+        pom.licenses(this::customizeLicenses);
+        pom.developers(this::customizeDevelopers);
+        pom.scm((scm) -> customizeScm(scm, project));
+        pom.issueManagement(this::customizeIssueManagement);
+    }
+
+    /**
+     * 定义 issueManagement
+     *
+     * @param issueManagement issueManagement
+     */
+    private void customizeIssueManagement(MavenPomIssueManagement issueManagement) {
+        issueManagement.getSystem().set(PomConstant.ISSUE_SYSTEM);
+        issueManagement.getUrl().set(PomConstant.ISSUE_URL);
+    }
+
+    /**
+     * 定义 scm
+     *
+     * @param scm scm
+     * @param project project
+     */
+    private void customizeScm(MavenPomScm scm, Project project) {
+        scm.getConnection().set(PomConstant.GIT_SCM_CONNECTION);
+        scm.getDeveloperConnection().set(PomConstant.GIT_SCM_DEVELOPER_CONNECTION);
+        scm.getUrl().set(PomConstant.GIT_URL);
+    }
+
+    /**
+     * 定义开发者
+     *
+     * @param developers developers
+     */
+    private void customizeDevelopers(MavenPomDeveloperSpec developers) {
+        developers.developer(developer -> {
+            developer.getName().set(PomConstant.DEVELOPER_NAME);
+            developer.getEmail().set(PomConstant.DEVELOPER_EMAIL);
+            developer.getOrganization().set(PomConstant.POM_ORGANIZATION_NAME);
+            developer.getOrganizationUrl().set(PomConstant.POM_ORGANIZATION_URL);
+        });
+    }
+
+    /**
+     * 定义 licenses
+     *
+     * @param licenses licenses
+     */
+    private void customizeLicenses(MavenPomLicenseSpec licenses) {
+        licenses.license(licence -> {
+            licence.getName().set(PomConstant.LICENSE_NAME);
+            licence.getUrl().set(PomConstant.LICENSE_URL);
+        });
+    }
+
+    /**
+     * 定义组织
+     *
+     * @param organization organization
+     */
+    private void customizeOrganization(MavenPomOrganization organization) {
+        organization.getName().set(PomConstant.POM_ORGANIZATION_NAME);
+        organization.getUrl().set(PomConstant.POM_ORGANIZATION_URL);
+    }
+}
